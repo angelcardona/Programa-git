7cdef12f23022aaeb8b7c3d459a5d6de903a87ba
angelcardona <miguel.acardona98@gmail.com>
2025-04-20 20:36:47 -0500

    Modificated function

---
+++ git.py
@@ -0,0 +1,7 @@
+def sum():
+    print("This function helpful you to plus two numbers")
+    x=float(input("Please enter a number : "))
+    y=float(input("Please enter a number : "))
+    c=x+y
+    print(f"The result is {c}")
+sum()
